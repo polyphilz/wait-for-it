44ed2a87534da39c8d375c43ae7667ae92f1a9ba
Rohan Bansal <rohanarunbansal@gmail.com>
2021-03-26 19:16:26 -0400

    Set initial value of input checkbox based off key in localStorage

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 const div = document.querySelector("div");
 const input = document.querySelector("input[type=checkbox]");
 
+chrome.storage.sync.get(["wait-for-it-hn"], (result) => {
+  if (!result["wait-for-it-hn"]) return;
+  input.checked = true;
+});
+
 input.addEventListener("change", (e) => {
   div.textContent = e.target.checked ? "On" : "Off";
   chrome.storage.sync.set({ "wait-for-it-hn": e.target.checked }, () => {
